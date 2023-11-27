2f34b542fb6b68966a593ae241f936d1839dd860
Rui Duarte <ruiduarte.rrd@RuiPC.Home>
2023-11-27 11:59:00 +0000

    check

--- get_next_line.c
+++ get_next_line.c
@@ -3,31 +3,118 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ruirodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ruiduarte.rrd <ruiduarte.rrd@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:51:55 by ruirodri          #+#    #+#             */
-/*   Updated: 2023/11/18 21:43:03 by ruirodri         ###   ########.fr       */
+/*   Updated: 2023/11/26 11:06:53 by ruiduarte.r      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+//#include "get_next_line.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-char *ft_get_next_line(int fd)
+int	ft_clean(char *str)
 {
-	// so pode ser usada dentro da funcao get_next_line
-	static t_list	*list;
-	char			*next_line;
-	
-	list = NULL;
-	// read(int fildes, void *buffer, size_t nbytes); #include <unistd.h>
-	// verifica se o ficheiro pode ser aberto, caso nao de read retorna -1
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
-		return (NULL);
-	
+	int	i;
+	int	j;
+	int	is_nl;
+
+	i = 0;
+	j = 0;
+	is_nl = 0;
+	while (str[i])
+	{
+		if (is_nl)
+			str[j++] = str[i];
+		if (str[i] == '\n')
+			is_nl = 1;
+		str[i++] = '\0';
+	}
+	return (is_nl);
 }
 
-int	main(void)
+
+size_t	ft_strlen(char *s)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (s && s[i] && s[i] != '\n')
+		i++;
+	if (s && s[i] == '\n')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*joined;
+	int		i;
+	int		j;
+
+	if (s2[0] == '\0')
+		return (0);
+	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!joined)
+		return (0);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j] && s2[j] != '\n')
+		joined[i++] = s2[j++];
+	if (s2[j] == '\n')
+		joined[i++] = '\n';
+	joined[i] = '\0';
+	if (s1)
+		free(s1);
+	return (joined);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	line[BUFFER_SIZE + 1];
+	char		*next_line;
+	int			i;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || FOPEN_MAX < fd)
+		return (NULL);
+	next_line = ft_strjoin(0, line);
+	if (ft_clean(line) > 0)
+		return (next_line);
+	i = read(fd, line, BUFFER_SIZE);
+	if (i < 0)
+	{
+		free(next_line);
+		return (NULL);
+	}
+	while (i > 0)
+	{
+		next_line = ft_strjoin(next_line, line);
+		if (ft_clean(line) > 0)
+			break ;
+		i = read(fd, line, BUFFER_SIZE);
+	}
+	return (next_line);
+}
+
+int main()
+{
+    char *temp;
+	int	i = 1;
+	int fd = open("file.txt", O_RDONLY);
+
+    while ((temp = get_next_line(fd)) != NULL)
+    {
+        printf("%d -> %s",i++, temp);
+        free(temp);
+    }
+	printf("\n");
 	return (0);
 }
